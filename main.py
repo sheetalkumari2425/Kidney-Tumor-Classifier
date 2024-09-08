ce223bf1884d16a21e53595070408f7826823066
sheetalkumari2425 <sheetal.kumari@2425>
2024-09-09 02:19:52 +0530

    Completed data ingestion modular coding

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src.KidneyTumorClassifier import logger
+from KidneyTumorClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom logs")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f"------ Stage {STAGE_NAME} started ------")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f"------ Stage {STAGE_NAME} completed successfully ------")
+except Exception as e:
+    logger.exception(e)
+    raise e
