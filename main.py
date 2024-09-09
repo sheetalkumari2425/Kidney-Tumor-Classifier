68e45deab3fedd5bb67e3fb00b10c32d936ee76b
sheetalkumari2425 <sheetal.kumari@2425>
2024-09-09 15:40:14 +0530

    Completed model taining

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.KidneyTumorClassifier import logger
 from KidneyTumorClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from KidneyTumorClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from KidneyTumorClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -13,6 +14,7 @@ except Exception as e:
     raise e
 
 
+
 STAGE_NAME = "Prepare base model"
 try: 
    logger.info(f"*******************")
@@ -23,3 +25,17 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f"--------- stage {STAGE_NAME} started ---------")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f"--------- stage {STAGE_NAME} completed -----------\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+     
--- src/KidneyTumorClassifier/config/configuration.py
+++ src/KidneyTumorClassifier/config/configuration.py
@@ -1,12 +1,15 @@
 from KidneyTumorClassifier.constants import *
 from KidneyTumorClassifier.utils.common import read_yaml, create_directories
-from KidneyTumorClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
+from KidneyTumorClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig
+import os
 
 class ConfigurationManager:
+
     def __init__(self, config_filepath = CONFIG_FILE_PATH, params_filepath = PARAMS_FILE_PATH):
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
         create_directories([self.config.artifacts_root])
+
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
@@ -14,6 +17,7 @@ class ConfigurationManager:
         data_ingestion_config = DataIngestionConfig(root_dir=config.root_dir,source_URL=config.source_URL,
                                                     local_data_file=config.local_data_file,unzip_dir=config.unzip_dir)
         return data_ingestion_config
+    
     
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
         config = self.config.prepare_base_model
@@ -29,3 +33,23 @@ class ConfigurationManager:
             params_classes=self.params.CLASSES
         )
         return prepare_base_model_config
+    
+
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "kidney-ct-scan-image")
+        create_directories([Path(training.root_dir)])
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+        return training_config
